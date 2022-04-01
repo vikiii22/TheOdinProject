5813960d3ae44656f272d7230824244ffaa91d23
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-04-01 12:53:02 +0200

    npm

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,10 +1,18 @@
 const { dirname } = require('path');
-const path=require('path');
+const path = require('path');
 
-module.exports={
+module.exports = {
     entry: './src/index.js',
     output: {
-        filename:'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.css$/i',
+                use: ["style-loader", "css-loader"],
+            },
+        ],
     },
 };
